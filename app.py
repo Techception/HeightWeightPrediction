6ab368651cd1f179e6d573db921b65e00c662b71
techception <alexhall1993@hotmail.co.uk>
2024-03-16 13:25:41 +0000

    working site

--- app.py
+++ app.py
@@ -8,8 +8,7 @@ app = Flask(__name__)
 def hello_world():
     return "<p>Hello, World!</p>"
     
-app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 80)))
+#app.run(host=os.getenv('IP', '0.0.0.0'), port=int(os.getenv('PORT', 80)))
     
 if __name__ == '__main__':
-    app.run()
-    app.debug(True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
